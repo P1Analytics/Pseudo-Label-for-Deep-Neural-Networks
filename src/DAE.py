1794bd09490b6a496f2cc00ef0d4dd09455ef812
Zhu Na <nanazhu@users.noreply.github.com>
2017-10-02 10:32:00 +0200

    dropout on backwards phase

--- src/DAE.py
+++ src/DAE.py
@@ -14,20 +14,20 @@ class Network(object):
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
 
-    def backprop_with_mini_batch(self, mini_batch, eta=3, epoch=0, pl=False, DAE=False):
+    def backprop_with_mini_batch(self, mini_batch, DAE = False):
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         for x, y in mini_batch:
-            delta_nabla_b, delta_nabla_w = self.backprop(x, y, epoch, pl, DAE)
+            delta_nabla_b, delta_nabla_w = self.backprop(x, y, DAE)
             nabla_b = [nb + dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
             nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-        self.weights = [w - (eta / len(mini_batch)) * nw for w, nw in zip(self.weights, nabla_w)]
-        self.biases = [b - (eta / len(mini_batch)) * nb for b, nb in zip(self.biases, nabla_b)]
 
-    def backprop(self, x, y, epoch=0, pl=False, DAE=False):
+        return nabla_w, nabla_b
 
-        nabla_b = [np.zeros(b.shape) for b in self.biases]
-        nabla_w = [np.zeros(w.shape) for w in self.weights]
+    def backprop(self, x, y, DAE = False):
+
+        delta_nabla_b = [np.zeros(b.shape) for b in self.biases]
+        delta_nabla_w = [np.zeros(w.shape) for w in self.weights]
 
         # feedforward phase
         activation = x
@@ -35,75 +35,49 @@ class Network(object):
         zs = []
         layer = 0
         for b, w in zip(self.biases, self.weights):
-            # TODO drop out here(9) drop out making things worse not better  drop out rates: 50 % dropout for all hidden units;  20 % dropout for visible units
-            # activation = np.multiply(activation, dropout(0.2, np.shape(activation)))
+            if layer == 0:
+                mask_1 = dropout(0.5, np.shape(activation))
+                activation = np.multiply(activation, mask_1)
+            else:
+                mask_2 = dropout(0.2, np.shape(activation))
+                activation = np.multiply(activation, mask_2)
+            layer += 1
             z = np.dot(w, activation) + b
             zs.append(z)
-            if layer == 0:
-                activation = sigmoid(z)
-                # TODO use differnet active functions return all WRONG label ! WHY ??
-                # activation = rectifier(z)
-            else:
-                activation = sigmoid(z)
-            layer += 1
+            activation = sigmoid(z)
             activations.append(activation)
-
 
         # backward phase
         if DAE:
             y = x
-
-        # delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1]) # QuadraticCost
         delta = self.cost_derivative(activations[-1], y)
-
-        # TODO something wrong here !
-        if pl:
-            # TODO when will be both pseudo label and DAE they are in different phase
-            delta *= self.alfaCoefficient(epoch, DAE)
-        nabla_b[-1] = delta
-        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+        delta_nabla_b[-1] = delta
+        delta_nabla_w[-1] = np.dot(delta, np.multiply(activations[-2],mask_2).transpose())
 
         for l in range(2, self.num_layers):
             z = zs[-l]
             delta = np.dot(self.weights[-l + 1].transpose(), delta) * sigmoid_prime(z)
-            # TODO different active function in the hidden layer, different prime(3)(BP2 in Ref)
-            # delta = np.dot(self.weights[-l + 1].transpose(), delta) * rectifier_prime(z)
-            nabla_b[-l] = delta
-            nabla_w[-l] = np.dot(delta, activations[-l - 1].transpose())
+            delta_nabla_b[-l] = delta
+            delta_nabla_w[-l] = np.dot(delta,np.multiply(activations[-l - 1],mask_1).transpose())
 
-        return nabla_b, nabla_w
+        return delta_nabla_b, delta_nabla_w
 
     def predict(self, a):
-        layer = 0
         for b, w in zip(self.biases, self.weights):
             z = np.dot(w, a) + b
-            if layer == 0:
-                a = sigmoid(z)
-                # TODO different active function in the hidden layer
-                # a = rectifier(z) # for hidden layers
-            else:
-                a = sigmoid(z)  # for output layer
-            layer += 1
+            a = sigmoid(z)
         return a
 
     def evaluate(self, test_data):
         test_results = [(np.argmax(self.predict(x)), y) for (x, y) in test_data]
-        return sum(int(x == y) for (x, y) in test_results)
+        acc = sum(int(x == y) for (x, y) in test_results)
+        error = 1 - acc / len(test_results)
+        return error*100
+
 
     def cost_derivative(self, output_activations, y):
-        return output_activations - y
+        return (output_activations - y)/ np.log(10)
 
-    def alfaCoefficient(self, currentEpoch, DAE=False):
-        if DAE:
-            epochT1, epochT2 = 200, 800
-        else:
-            epochT1, epochT2 = 100, 600
-        if currentEpoch < epochT1:
-            return 0
-        elif epochT1 <= currentEpoch & currentEpoch < epochT2:
-            return ((currentEpoch - epochT1) * 0.4) / epochT2 - epochT1
-        elif epochT2 <= currentEpoch:
-            return 3
 
     def SGD_DAE(self, training_data, epochs, mini_batch_size, eta, validation_data=None, test_data=None):
 
@@ -112,8 +86,17 @@ class Network(object):
         for j in range(epochs):
             random.shuffle(training_data)
             mini_batches = [training_data[k:k + mini_batch_size] for k in range(0, len(training_data), mini_batch_size)]
+            delta_w = [np.zeros(w.shape) for w in self.weights]
+            delta_b = [np.zeros(b.shape) for b in self.biases]
             for mini_batch in mini_batches:
-                self.backprop_with_mini_batch(mini_batch, eta, DAE=True)
+                nabla_w, nabla_b = self.backprop_with_mini_batch(mini_batch, DAE=True)
+                delta_w = [w_pri * p(j) - nw * (1 - p(j)) * learning_rate(eta, j) for w_pri, nw in
+                           zip(delta_w, nabla_w)]
+                delta_b = [b_pri * p(j) - nb * (1 - p(j)) * learning_rate(eta, j) for b_pri, nb in
+                           zip(delta_b, nabla_b)]
+                self.weights = [w + d_w for w, d_w in zip(self.weights, delta_w)]
+                self.biases = [b + d_b for b, d_b in zip(self.biases, delta_b)]
+
 
         input_layer = self.sizes[0]
         hidden_layer = self.sizes[1]
@@ -122,12 +105,20 @@ class Network(object):
         secondNetwork.weights[0] = self.weights[0]
         secondNetwork.biases[0] = self.biases[0]
 
-        print("# Fine tuning supervised learning")
+        print("# Fine tuning supervised learning: pseudo label")
         for j in range(epochs):
             random.shuffle(training_data)
             mini_batches = [training_data[k:k + mini_batch_size] for k in range(0, len(training_data), mini_batch_size)]
+            delta_w = [np.zeros(w.shape) for w in secondNetwork.weights]
+            delta_b = [np.zeros(b.shape) for b in secondNetwork.biases]
             for mini_batch in mini_batches:
-                secondNetwork.backprop_with_mini_batch(mini_batch, eta)
+                nabla_w, nabla_b= secondNetwork.backprop_with_mini_batch(mini_batch)
+                delta_w = [w_pri * p(j) - nw * (1 - p(j)) * learning_rate(eta, j) for w_pri, nw in
+                           zip(delta_w, nabla_w)]
+                delta_b = [b_pri * p(j) - nb * (1 - p(j)) * learning_rate(eta, j) for b_pri, nb in
+                           zip(delta_b, nabla_b)]
+                secondNetwork.weights = [w + d_w for w, d_w in zip(secondNetwork.weights, delta_w)]
+                secondNetwork.biases = [b + d_b for b, d_b in zip(secondNetwork.biases, delta_b)]
 
             # pseudo label session
             labels = []
@@ -138,31 +129,49 @@ class Network(object):
 
         validation_array_results = np.array(validation_results)
         pseudo_labels = []
+        validation_results = []
         for i in range(validation_array_results.shape[1]):
             a = Counter(validation_array_results[:, i]).most_common(1)[0][0]
-            label = vectorized_result(a)
-            pseudo_labels.append(label)
-            data_pseudo_label = list(zip([x for x, y in validation_data], pseudo_labels))
+            validation_results.append(a)
+            pseudo_labels.append(vectorized_result(a))
+        X = [x for x, y in validation_data]
+        validation_PL_Label = list(zip(X, pseudo_labels))
+
 
         print("# train the network again with (training data+ real label) & (validate data + pseudo label)")
         for j in range(epochs):
             random.shuffle(training_data)
-            mini_batches = [training_data[k:k + mini_batch_size] for k in range(0, len(training_data), mini_batch_size)]
-            for mini_batch in mini_batches:
-                secondNetwork.backprop_with_mini_batch(mini_batch, eta)
+            mini_batches = [training_data[k:k + mini_batch_size] for k in
+                            range(0, len(training_data), mini_batch_size)]
 
-            random.shuffle(data_pseudo_label)
+            random.shuffle(validation_PL_Label)
             mini_valid_batch_size = 256
-            mini_valid_batches = [data_pseudo_label[k:k + mini_valid_batch_size] for k in
+            mini_valid_batches = [validation_PL_Label[k:k + mini_valid_batch_size] for k in
                                   range(0, len(validation_data), mini_valid_batch_size)]
-            for mini_vilid_batch in mini_valid_batches:
-                secondNetwork.backprop_with_mini_batch(mini_vilid_batch, eta, epoch=j, pl=True)
+
+            delta_w = [np.zeros(w.shape) for w in secondNetwork.weights]
+            delta_b = [np.zeros(b.shape) for b in secondNetwork.biases]
+            for i in range(0, len(mini_batches)):
+                nabla_w, nabla_b = secondNetwork.backprop_with_mini_batch(mini_batches[i])
+                nabla_w = [x / mini_batch_size for x in nabla_w]
+                nabla_b = [x / mini_batch_size for x in nabla_b]
+                if i < len(mini_valid_batches):
+                    nabla_w_pl, nabla_b_pl = secondNetwork.backprop_with_mini_batch(mini_valid_batches[i])
+                    nabla_w_pl = [x / mini_valid_batch_size for x in nabla_w_pl]
+                    nabla_b_pl = [x / mini_valid_batch_size for x in nabla_b_pl]
+                    nabla_w += nabla_w_pl
+                    nabla_b += nabla_b_pl
+
+                delta_w = [w_pri * p(j) - nw * (1 - p(j)) * learning_rate(eta, j) for w_pri, nw in
+                               zip(delta_w, nabla_w)]
+                delta_b = [b_pri * p(j) - nb * (1 - p(j)) * learning_rate(eta, j) for b_pri, nb in
+                               zip(delta_b, nabla_b)]
+                secondNetwork.weights = [w + d_w for w, d_w in zip(secondNetwork.weights, delta_w)]
+                secondNetwork.biases = [b + d_b for b, d_b in zip(secondNetwork.biases, delta_b)]
+
 
             if test_data:
-                n_test = len(test_data)
-                print("Epoch {0}: {1} / {2}".format(j, secondNetwork.evaluate(test_data), n_test))
-            else:
-                print("Epoch {0} complete".format(j))
+                print(secondNetwork.evaluate(test_data),"%")
 
 
 def sigmoid(z):
@@ -269,12 +278,44 @@ def split_by_label(dataset, num_per_label):
             continue
 
     random.shuffle(new_dataset)
+    print(counter)
     return new_dataset
 
 
-if __name__ == "__main__":
-    training_data, validation_data, test_data = load_data_wrapper();
+def learning_rate(eta, epoch):
+    return eta * (0.998 ** epoch)
 
-    # DROP NN + PL + DAE
-    DAE = Network([784, 5000, 784])
-    DAE.SGD_DAE(training_data, epochs=20, mini_batch_size=32, eta=3.0, validation_data=validation_data,test_data=test_data)
+
+def p(t):
+    pf = 0.99
+    pi = 0.5
+    T = 500
+    if t < T:
+        return t / T * pf + (1 - t / T) * pi
+    else:
+        return pf
+
+
+def alfaCoefficient(currentEpoch, DAE=False):
+    if DAE:
+        epochT1, epochT2 = 200, 800
+    else:
+        epochT1, epochT2 = 100, 600
+    if currentEpoch < epochT1:
+        return 0
+    elif epochT1 <= currentEpoch & currentEpoch < epochT2:
+        return ((currentEpoch - epochT1) * 0.4) / epochT2 - epochT1
+    elif epochT2 <= currentEpoch:
+        return 3
+
+if __name__ == "__main__":
+    training_data, validation_data, test_data = load_data_wrapper()
+
+    loop = 15
+    learning = 1.5
+    print("eta = ", learning, "epochs = ", loop)
+
+    for i in (10,0):
+        training_data_small = split_by_label(training_data, num_per_label=i)
+        Neural = Network([784, 5000, 784])
+        Neural.SGD_DAE(training_data_small, epochs=loop, mini_batch_size=32, eta=learning, validation_data=validation_data,test_data=test_data)
